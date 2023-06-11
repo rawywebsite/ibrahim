2b48119450cb4cf2dbf676762e9b034a4befe48f
AhmedWaled <67336269+LiNePasha@users.noreply.github.com>
2023-06-12 00:37:23 +0300

    Update index.vue

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -20,61 +20,45 @@
               </div>
               </div>
   
-          <div class="blog__area grey-bg pt-100 pb-60">
-              <div class="container">
-                  <div class="row">
-                      <div class="col-xl-12 wow tpfadeUp" data-wow-duration=".9s" data-wow-delay=".3s">
-                          <div class="blog__title text-center pb-15">
-                              <h4 class="section-title">Check Latest Blog</h4>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="row">
-                      <div class="col-xl-6 col-lg-6 col-md-6 mb-50 wow tpfadeUp" data-wow-duration=".9s"
-                          data-wow-delay=".5s">
-                          <div class="blog__item">
-                              <div class="blog__item-img fix">
-                                  <a href="blog.html"><img src="/assets/img/blog/blog-1.png" alt=""></a>
-                              </div>
-                              <div class="blog__item-content">
-                                  <div class="blog__category pb-35 d-flex justify-content-between">
-                                      <h4 class="blog__meta">DEVELOPMENT</h4>
-                                      <h4 class="blog__date">FEB 3, 2023</h4>
-                                  </div>
-                                  <div class="blog__content">
-                                      <h4 class="blog__title-sm"><a href="blog.html">OVERRIDE THE DIGITAL DIVIDE WITH
-                                              ADD<br>
-                                              RELATIONSHIP BUILD</a></h4>
-                                      <p>Nancy boy Charles down the pub get stuffed mate easy peasy brown bread car boot
-                                          squiffy loo, blimey</p>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div class="col-xl-6 col-lg-6 col-md-6 mb-50 wow tpfadeUp" data-wow-duration=".9s"
-                          data-wow-delay=".7s">
-                          <div class="blog__item">
-                              <div class="blog__item-img fix">
-                                  <a href="blog.html"><img src="/assets/img/blog/blog-2.png" alt=""></a>
-                              </div>
-                              <div class="blog__item-content">
-                                  <div class="blog__category pb-35 d-flex justify-content-between">
-                                      <h4 class="blog__meta">DESIGENER</h4>
-                                      <h4 class="blog__date">FEB 3, 2023</h4>
-                                  </div>
-                                  <div class="blog__content">
-                                      <h4 class="blog__title-sm"><a href="blog.html">There are many variations of passages
-                                              of <br>
-                                              Lorem Ipsum available</a></h4>
-                                      <p>Nancy boy Charles down the pub get stuffed mate easy peasy brown bread car boot
-                                          squiffy loo, blimey</p>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
+ <div class="blog__area grey-bg pt-100 pb-60">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12 wow tpfadeUp" data-wow-duration=".9s" data-wow-delay=".3s">
+                        <div class="blog__title text-center pb-15">
+                            <h4 class="section-title">مقالات موديل مصر</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6 col-md-6 mb-50 wow tpfadeUp" data-wow-duration=".9s"
+                        data-wow-delay=".5s">
+                        <div class="blog__item">
+                            <div class="blog__item-img fix">
+                                <NuxtLink to="/blog/model-masr-with-3nba"><img src="/assets/img/blog/1.png" alt="k"></NuxtLink>
+                            </div>
+                            <div class="blog__item-content">
+                                <div class="blog__content text-center">
+                                    <h4 class="blog__title-sm"><NuxtLink to="/blog/model-masr-with-3nba">اصغر موديل في مصر وعنبة</NuxtLink></h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 mb-50 wow tpfadeUp" data-wow-duration=".9s"
+                        data-wow-delay=".7s">
+                        <div class="blog__item">
+                            <div class="blog__item-img fix">
+                                <NuxtLink href="/blog/model-masr-with-3mrosy"><img src="/assets/img/blog/2.png" alt="k"></NuxtLink>
+                            </div>
+                            <div class="blog__item-content">
+                                <div class="blog__content text-center">
+                                    <h4 class="blog__title-sm"><NuxtLink href="/blog/model-masr-with-3mrosy">اصغر موديل في مصر وعمرو عمروسي</NuxtLink></h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
   
     </div>
   </template>

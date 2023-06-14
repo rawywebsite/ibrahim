eb877e9e9e46b018a40c832ffb5663541a3c1be5
AhmedWaled <67336269+LiNePasha@users.noreply.github.com>
2023-06-14 23:28:24 +0300

    Add files via upload

---
+++ error.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+
+const errors = useError()
+useHead({
+  title: 'Error',
+  meta: [
+      { name: 'description', content: 'My amazing site.' },
+      { name: 'robots', content: 'noindex, follow' },
+  ],
+  script: [
+            { src: '/assets/js/jquery.js', body: true },
+            { src: '/assets/js/wow.js', body: true },
+            { src: '/assets/js/main.js', body: true },
+          ],
+})
+</script>
+
+
+<template>
+  <div>
+        <div class="banner__area p-relative z-index-3 banner__space blue-bg pb-130 pt-60">
+            <div class="banner__text">
+            <h2 class="banner__big-text">فاشون</h2>
+            </div>
+            <div class="container">
+            <div class="row align-items-center">
+            <div class="col-xl-6 col-lg-6 col-md-6">
+            <div class="banner__section text-center">
+            <h2 class="text-danger"><span>.</span>  404 </h2>
+            <h2><span>.</span>الصفحة غير موجودة </h2>
+            <NuxtLink class="main-btn" to="/"><span>الصفحة الرئيسية</span></NuxtLink>
+            </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6">
+            <div class="banner__main-img text-center">
+              <nuxt-img alt="صورة error في موقع اصغر موديل في مصر" loading="lazy" fit="cover" provider="cloudinary" src="/ib_error.webp"/>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,17 +5,55 @@ export default defineNuxtConfig({
           charset: 'utf-8',
           viewport: 'width=device-width, initial-scale=1',
           link: [
-            { rel: 'shortcut icon', type: 'image/x-icon', href: '/assets/img/favicon.png' },
+            { rel: 'icon', type: 'image/x-icon', href: '/assets/img/favicon.ico' },
+            { rel: 'shortcut icon', type: 'image/x-icon', href: '/assets/img/favicon.ico' },
+            { rel: 'apple-touch-icon', href: '/assets/img/logo/logo.png' },
+            { rel: 'apple-touch-icon-precomposed', href: '/assets/img/logo/logo.png' },
             { rel: 'stylesheet', href: '/assets/css/bootstrap.min.css' },
             { rel: 'stylesheet', href: '/assets/css/animate.css' },
             { rel: 'stylesheet', href: '/assets/css/custom-animation.css' },
-            { rel: 'stylesheet', href: '/assets/css/nice-select.css' },
             { rel: 'stylesheet', href: '/assets/css/flaticon.css' },
             { rel: 'stylesheet', href: '/assets/css/meanmenu.css' },
             { rel: 'stylesheet', href: '/assets/css/font-awesome-pro.css' },
             { rel: 'stylesheet', href: '/assets/css/magnific-popup.css' },
             { rel: 'stylesheet', href: '/assets/css/spacing.css' },
-            { rel: 'stylesheet', href: '/assets/css/style.css' },
+        ],
+        meta: [
+          { name: 'theme-color', content: '#EBEBF9' },
+          { name: 'secound-theme-color', content: '#2E2AEB' },
+          { name: 'HandheldFriendly', content: 'True' },
+          { name: 'MobileOptimized', content: '320' },
+          { name: 'apple-mobile-web-app-capable', content: 'yes' },
+          { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+          { name: 'robots', content: 'max-snippet:-1, max-image-preview:large, max-video-preview:-1' },
+          { name: 'robots', content: 'index, follow' },
+          { name: 'og:type', content: 'website' },
+          { name: 'og:image', content: 'https://res.cloudinary.com/dzxbbqq4l/image/upload/v1686569747/logo_kawrjy.png' },
+          { name: 'twitter:card', content: 'summary_large_image' },
+          { name: 'twitter:site', content: '@اصفر_موديل_في_مصر' },
+          { name: 'twitter:creator', content: '@اصفر_موديل_في_مصر' },
+          { name: 'twitter:title', content: '@اصفر_موديل_في_مصر' },
+          { name: 'twitter:image:src', content: 'https://res.cloudinary.com/dzxbbqq4l/image/upload/v1686569747/logo_kawrjy.png' },
+          { name: 'note', content: 'ابراهيم افضل موديل ازياء في مصر' },
+          { name: 'distribution', content: 'global' },
+          { name: 'classification', content: 'index, follow' },
+          { name: 'author', content: 'ابراهيم جمال' },
+          { name: 'owner', content: 'https://ibmodel.lineitsolutions.com' },
+          { name: 'copyright', content: 'https://lineitsolutions.com' },
+          { name: 'web_author', content: 'Ahmed Waled' },
+          { name: 'MSSmartTagsPreventParsing', content: 'true' },
+          { name: 'operator', content: 'ابراهيم' },
+          { name: 'contactOrganization', content: 'https://ibmodel.lineitsolutions.com' },
+          { name: 'contactStreetAddress1', content: 'المرج' },
+          { name: 'contactStreetAddress2', content: 'عباس العقاد' },
+          { name: 'contactCity', content: 'cairo' },
+          { name: 'contactCountry', content: 'Egypt' },
+          { name: 'contactPhoneNumber', content: '+201148774722' },
+          { name: 'contactNetworkAddress', content: 'https://ibmodel.lineitsolutions.com' },
+          { name: 'msapplication-starturl', content: 'https://ibmodel.lineitsolutions.com' },
+          { name: 'msapplication-tooltip', content: 'موقع ابراهيم اصغر موديل في مصر' },
+          { name: 'DC.Creator', content: 'Developed By Ahmed Waled' },
+          { name: 'format-detection', content: 'telephone=no' },
         ],
         script: [
           // { src: '/assets/js/yt.js', body: true },
@@ -29,9 +67,15 @@ export default defineNuxtConfig({
         provider: 'cloudinary',
         cloudinary: {
           baseURL: 'https://res.cloudinary.com/dzxbbqq4l/image/upload/v1686515220',
-        }
+        },
       },
-      router: {
-
+      extends: [
+        'nuxt-seo-kit'
+      ],
+      runtimeConfig: {
+        public: {
+          siteName: 'ابراهيم جمال : افضل موديل ازياء في مصر',
+          language: 'ar-ae'
+        }
       }
 })

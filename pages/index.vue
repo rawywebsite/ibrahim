849a39aaeabf1ad7f8ed273ac2a49e29e5a9f05b
AhmedWaled <67336269+LiNePasha@users.noreply.github.com>
2023-06-14 22:42:42 +0300

    Delete pages directory

--- pages/index.vue
+++
@@ -1,313 +0,0 @@
-<template>
-    <div>
-            <div dir="rtl" class="hero__area blue-bg pt-60 p-relative fix">
-            <div class="hero__shape-1 d-none d-lg-block">
-                <img src="/assets/img/banner/hero-shape-4.png" alt="k">
-            </div>
-            <div class="hero__shape-2 d-none d-sm-block">
-                <img src="/assets/img/banner/project-shape-3.png" alt="k">
-            </div>
-            <div class="hero__shape-3 d-none d-lg-block">
-                <img src="/assets/img/banner/hero-shape-5.1.png" alt="k">
-            </div>
-            <div class="hero__gray-bg d-none d-lg-block"></div>
-            <div class="circle-animation banner d-none d-lg-block">
-                <span class="it-circle-1"></span>
-                <span class="it-circle-2"></span>
-            </div>
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-xl-7 col-lg-7 col-md-8">
-                        <div class="hero__left-side z-index-3">
-                            <div class="hero__setion-title pb-15">
-                                <h4 class="section-subtitle wow fadeInRight">اهلا بكم</h4>
-                                <h2 class="hero-title wow fadeInRight" ><div class="rbt-new-badge rbt-new-badge-one">
-                                    أصغر مودل 
-                                    في مصر <img src="/assets/img/71984.png" alt="">
-                                </div></h2>
-                            </div>
-                            <div class="hero__left-text mt-3 wow tpfadeUp" data-wow-duration=".9s" data-wow-delay=".3s">
-                                <p>صديق للموضة 🕴🔥 , عارض أزياء <i class="fal fa-user-tie"></i></p>
-                            </div>
-                            
-                            <div class="hero__left-button wow tpfadeUp" data-wow-duration=".9s" data-wow-delay=".6s">
-                                <a class="main-btn" href="contact.html"><span>أبراهيم والمشاهير</span></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-5 col-lg-5 col-md-4">
-                        <div class="hero__right-side text-center text-lg-start p-relative">
-                            <div class="hero__right-img z-index-3">
-                                <img src="/assets/img/Untitled_design__9_-removebg-preview.png" alt="k">
-                                <div class="hero__social-box d-none d-xl-block">
-                                    <div class="hero__social">
-                                        <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                        <a href="#"><i class="fab fa-instagram"></i></a>
-                                        <a href="#"><i class="fab fa-twitter"></i></a>
-                                        <a href="#"><i class="fab fa-pinterest-p"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    
-                </div>
-            </div>
-        </div>
-
-
-        <div class="project__area grey-bg fix p-relative pt-100">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-12">
-                        <div class="service__section text-center">
-                            <h4 class="section-title wow tpfadeUp" data-wow-duration=".9s" data-wow-delay=".3s">أبراهيم والمشاهير</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-xxl-4 col-xl-6 col-lg-6 col-md-6 mb-30 wow tpfadeUp" data-wow-duration=".9s"
-                        data-wow-delay=".5s">
-                        <div class="service__item text-center">
-                                <img src="/assets/img/stars/1.png" alt="k">
-                        </div>
-                    </div>
-                    <div class="col-xxl-4 col-xl-6 col-lg-6 col-md-6 mb-30 wow tpfadeUp" data-wow-duration=".9s"
-                        data-wow-delay=".5s">
-                        <div class="service__item text-center">
-                                <img src="/assets/img/stars/2.png" alt="k">
-                        </div>
-                    </div>
-                    <div class="col-xxl-4 col-xl-6 col-lg-6 col-md-6 mb-30 wow tpfadeUp" data-wow-duration=".9s"
-                        data-wow-delay=".5s">
-                        <div class="service__item text-center">
-                                <img src="/assets/img/stars/3.png" alt="k">
-                        </div>
-                    </div>
-                    <div class="col-xxl-4 col-xl-6 col-lg-6 col-md-6 mb-30 wow tpfadeUp" data-wow-duration=".9s"
-                        data-wow-delay="1s">
-                        <div class="service__item text-center">
-                            <div class="service__item-link">
-                                <a href="about.html">جميع صور أبراهيم مع المشاهير<i class="far fa-long-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <hr class="ibrahim_line">
-                <div class="row">
-                    <div class="col-xl-12">
-                        <div class="service__section text-center">
-                            <h4 class="section-title wow tpfadeUp" data-wow-duration=".9s" data-wow-delay=".3s">أبراهيم والموضة</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-xxl-4 col-xl-6 col-lg-6 col-md-6 mb-30 wow tpfadeUp" data-wow-duration=".9s"
-                        data-wow-delay=".5s">
-                        <div class="service__item text-center">
-                                <img src="/assets/img/fashion/1.jpg" alt="k">
-                        </div>
-                    </div>
-                    <div class="col-xxl-4 col-xl-6 col-lg-6 col-md-6 mb-30 wow tpfadeUp" data-wow-duration=".9s"
-                        data-wow-delay=".5s">
-                        <div class="service__item text-center">
-                                <img src="/assets/img/fashion/2.jpg" alt="k">
-                        </div>
-                    </div>
-                    <div class="col-xxl-4 col-xl-6 col-lg-6 col-md-6 mb-30 wow tpfadeUp" data-wow-duration=".9s"
-                        data-wow-delay=".5s">
-                        <div class="service__item text-center">
-                                <img src="/assets/img/fashion/3.jpg" alt="k">
-                        </div>
-                    </div>
-                    <div class="col-xxl-4 col-xl-6 col-lg-6 col-md-6 mb-30 wow tpfadeUp" data-wow-duration=".9s"
-                        data-wow-delay="1s">
-                        <div class="service__item text-center">
-                            <div class="service__item-link">
-                                <a href="about.html">جميع صور أبراهيم والموضة<i class="far fa-long-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="project__area grey-bg fix p-relative pt-100">
-                <div class="container">
-                    <div dir="rtl" class="row align-items-center">
-                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 mb-30 wow tpfadeUp" data-wow-duration=".9s"
-                        data-wow-delay=".5s">
-                            <div class="bg_ready"></div>
-                    </div>
-                        <div class="col-xl-6 col-lg-6 order-0 order-lg-1">
-                            <div class="project__item">
-                                <div class="project__text">
-                                    <h4 class="section-title wow tpfadeUp" data-wow-duration=".9s" data-wow-delay=".3s">
-                                        مستعد لعرض أزيائك ؟</h4>
-                                    <h5 style="line-height: 3rem; color: var(--it-common-black);" class="wow tpfadeUp" data-wow-duration=".9s" data-wow-delay=".5s">إذا كنت صاحب بيزنس أو براند ملابس وتحتاج صور بروفيشنال مع مودل  <br> تقدر تتواصل معي شخصيا رقمي : 01148774722 </h5>
-                                </div>
-                                <div class="project__button wow tpfadeUp" data-wow-duration=".9s" data-wow-delay=".9s">
-                                    <a class="main-btn" href="contact.html"><span>تواصل معي الأن</span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-        </div>
-
-
-        <div class="testimonial__area gray-bg pt-155">
-            <div class="container">
-                <div dir="rtl" class="row align-items-center">
-                    <div class="col-xl-5 col-lg-5 wow tpfadeLeft" data-wow-duration=".9s" data-wow-delay=".3s">
-                        <div class="testimonial__main-img text-center">
-                            <div class="testimonial__shape">
-                                <img src="/assets/img/testimonial/testi-shape.png" alt="k">
-                            </div>
-                            <img src="/assets/img/testimonial/Untitled_design__10_-removebg-preview.png" alt="k">
-                        </div>
-                    </div>
-                    <div class="col-xl-7 col-lg-7 wow tpfadeRight" data-wow-duration=".9s" data-wow-delay=".7s">
-                        <div class="testimonial__wrapper-box p-relative">
-                            <div class="testimonial__content-bg">
-                                <div class="swiper-container testimonial__slider-active">
-                                    <div class="swiper-wrapper">
-                                        <div class="swiper-slide">
-                                            <div class="testimonial__content-box">
-                                                <div class="row justify-content-center align-items-center">
-                                                    <div dir="rtl" class="col-lg-8">
-                                                        <div class="testimonial__icon">
-                                                            <span>
-                                                                <svg id="it-1" xmlns="http://www.w3.org/2000/svg"
-                                                                    xmlns:xlink="http://www.w3.org/1999/xlink" width="32"
-                                                                    height="24" viewbox="0 0 32 24">
-                                                                    <g id="reviews">
-                                                                        <g id="text_and_quotes-1" data-name="text and quotes">
-                                                                            <image id="straight-quotes" width="32" height="24"
-                                                                                xlink:href="data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAYCAYAAACbU/80AAAAfUlEQVRIiWP8LBf3n4E0wEhANUnmMZFoOdXBqANGHTDqgFEHjDqAVAfMoLL9M0hxAMjyLGpaDjKPWAfALCe15sRrHs/Dhf+JcQDNLAdxCDmAppYTcgDNLcfnALpYjssBdLMcmwPwKqa25egOoLvlyA4YEMthDhgwyxkYGBgALzFEqrPkeQsAAAAASUVORK5CYII=" />
-                                                                        </g>
-                                                                    </g>
-                                                                </svg>
-                                                            </span>
-                                                        </div>
-                                                        <div class="testimonial__text">
-                                                            <h5 class="mb-3">أصغر مودل في مصر ثقة في الله نجاح</h5>
-                                                            <a class="main-btn" href="contact.html"><span>شوف الفديو</span></a>
-                                                            <h4 class="testimonial__title-sm">- عنبة -</h4>
-                                                            <span>مغني وممثل</span>
-                                                        </div>
-                                                        
-                                                    </div>
-                                                    <div class="col-lg-4">
-                                                        <img src="/assets/img/stars/1.png" alt="k">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="swiper-slide">
-                                            <div class="testimonial__content-box">
-                                                <div class="row justify-content-center align-items-center">
-                                                    <div dir="rtl" class="col-lg-8">
-                                                        <div class="testimonial__icon">
-                                                            <span>
-                                                                <svg id="it-1" xmlns="http://www.w3.org/2000/svg"
-                                                                    xmlns:xlink="http://www.w3.org/1999/xlink" width="32"
-                                                                    height="24" viewbox="0 0 32 24">
-                                                                    <g id="reviews">
-                                                                        <g id="text_and_quotes-1" data-name="text and quotes">
-                                                                            <image id="straight-quotes" width="32" height="24"
-                                                                                xlink:href="data:img/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAYCAYAAACbU/80AAAAfUlEQVRIiWP8LBf3n4E0wEhANUnmMZFoOdXBqANGHTDqgFEHjDqAVAfMoLL9M0hxAMjyLGpaDjKPWAfALCe15sRrHs/Dhf+JcQDNLAdxCDmAppYTcgDNLcfnALpYjssBdLMcmwPwKqa25egOoLvlyA4YEMthDhgwyxkYGBgALzFEqrPkeQsAAAAASUVORK5CYII=" />
-                                                                        </g>
-                                                                    </g>
-                                                                </svg>
-                                                            </span>
-                                                        </div>
-                                                        <div class="testimonial__text">
-                                                            <h5 class="mb-3">أبراهيم أخويا وحبيبي أحلي مودل في مصر وأصغر مودل في مصر خلي بالك أصغر دي متقللش بالعكس دا راجل محترم جدا ربنا يكرمو أن شاء الله النجاح الدائم ليه </h5>
-                                                            <a class="main-btn" href="contact.html"><span>شوف الفديو</span></a>
-                                                            <h4 class="testimonial__title-sm">- عمر عمروسي -</h4>
-                                                            <span>ممثل</span>
-                                                        </div>
-                                                        
-                                                    </div>
-                                                    <div class="col-lg-4">
-                                                        <img src="/assets/img/stars/4.png" alt="k">
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="testimonial-slider-dots"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="blog__area grey-bg pt-100 pb-60">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-12 wow tpfadeUp" data-wow-duration=".9s" data-wow-delay=".3s">
-                        <div class="blog__title text-center pb-15">
-                            <h4 class="section-title">مقالات موديل مصر</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-xl-6 col-lg-6 col-md-6 mb-50 wow tpfadeUp" data-wow-duration=".9s"
-                        data-wow-delay=".5s">
-                        <div class="blog__item">
-                            <div class="blog__item-img fix">
-                                <NuxtLink to="/blog/model-masr-with-3nba"><nuxt-img 
-                                    provider="cloudinary"
-        src="/1_xtj0mt.png"
-        /></NuxtLink>
-                            </div>
-                            <div class="blog__item-content">
-                                <div class="blog__content text-center">
-                                    <h4 class="blog__title-sm"><NuxtLink to="/blog/model-masr-with-3nba">اصغر موديل في مصر وعنبة</NuxtLink></h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 col-lg-6 col-md-6 mb-50 wow tpfadeUp" data-wow-duration=".9s"
-                        data-wow-delay=".7s">
-                        <div class="blog__item">
-                            <div class="blog__item-img fix">
-                                <NuxtLink href="/blog/model-masr-with-3mrosy"><img src="/assets/img/blog/2.png" alt="k"></NuxtLink>
-                            </div>
-                            <div class="blog__item-content">
-                                <div class="blog__content text-center">
-                                    <h4 class="blog__title-sm"><NuxtLink href="/blog/model-masr-with-3mrosy">اصغر موديل في مصر وعمرو عمروسي</NuxtLink></h4>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-    useHead({
-        title: 'Home Page App',
-        meta: [
-            { name: 'description', content: 'My amazing site.' },
-            { name: 'robots', content: 'noindex, follow' },
-        ],
-        bodyAttrs: {
-            class: 'test'
-        },
-        script: [
-            { src: '/assets/js/jquery.js', body: true },
-            { src: '/assets/js/wow.js', body: true },
-            { src: '/assets/js/meanmenu.js', body: true },
-            { src: '/assets/js/main.js', body: true },
-          ],
-})  
-  </script>
